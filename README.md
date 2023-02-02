fde04e630ee2c56131561c77da53bcf1ccf9913b
Monde Sineke <nsineke@gmail.com>
2023-02-02 23:03:59 +0100

    init(*): 🎉  Create lesson code
    Pull code from stackblitz and prepare it for lesson

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
-# fed-js1-eventlisteners-events
+# Frontend web development Javascript lesson on eventlisteners & events
 
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-a4tfmn)
+## Resources
+
+- [Lesson](https://fed-vocational-astro-course.vercel.app/en/javascript-1/module-2/length-trim-eventlistener)
+- [Production deploy](https://vitejsvitea4tfmn-kso2--5173.local-credentialless.webcontainer.io)
+- [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-a4tfmn)
+
+## Description
+
+This is example code of using javascript. In this project we look at the following;
+
+- the length property and trim() method.
+- the addEventListener function.
+- the onkeyup event.
+- Event bubbling
+- Data attributes
+- Adding event handlers in a loop
+- mouseover and mouseout events
+- scroll event
+
+This project was bootstrapped with Vite.
+
+## Getting Started
+
+In the project directory, you can run:
+
+- install the project node module dependencies $`npm i`
+- Runs the app in the development mode. `npm run dev`
+- Open `http://127.0.0.1:5173/` to view it in the browser.
---
+++ libs/nav.js
@@ -0,0 +1,57 @@
+const navList = [
+  {
+    label: "Home",
+    path: "/",
+  },
+  {
+    label: "Home",
+    path: "/examples.html",
+    items: [
+      {
+        label: "Example 1",
+        path: "/length-trim.html",
+      },
+    ],
+  },
+];
+
+class Nav extends HTMLElement {
+  constructor() {
+    super();
+
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.innerHTML = `
+      <style>
+        :host {
+          display: block;
+          background: #eee;
+          padding: 10px;
+        }
+
+        .is-active {
+          color: green;
+        }
+      </style>
+
+      <nav>
+        <a href="/">Home</a>
+        <a href="/examples.html">Examples</a>
+        <a href="/examples/length-trim.html">Length & trim</a>
+        <a href="/examples/onkeyup-event.html">On key up event</a>
+      <nav>
+    `;
+
+    const currentUrl = window.location.pathname;
+
+    const links = this.shadowRoot.querySelectorAll("a");
+
+    links.forEach((link) => {
+      if (currentUrl.includes(link.pathname) && link.pathname !== "/") {
+        link.classList.add("is-active");
+        return;
+      }
+    });
+  }
+}
+
+customElements.define("my-nav", Nav);
